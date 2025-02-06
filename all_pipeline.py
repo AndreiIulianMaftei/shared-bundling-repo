02d588f3c01b6f3d119040062dc440df4b3a78f1
jacob-TUM <jacob_l._miller@hotmail.com>
2025-02-06 16:48:40 +0100

    Fixed small bug in all_pipeline

--- all_pipeline.py
+++ all_pipeline.py
@@ -2,6 +2,8 @@ from gen_graph import gen_graphs
 from bundle_pipeline import bundle_all
 from metrics_pipeline import all_metrics
 
+import os 
+if not os.path.isdir("inputs/"): os.mkdir("inputs")
 for i in range(10):
     gen_graphs(i)
 
