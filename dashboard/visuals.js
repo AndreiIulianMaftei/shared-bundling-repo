cca9fe0e5b035e5974bca68b3f3ba3b4abbba423
mwallinger <markus.wallinger@tum.de>
2025-03-29 17:36:54 +0100

    for andrei

--- dashboard/visuals.js
+++ dashboard/visuals.js
@@ -1,4 +1,5 @@
 const margin = 10;
+const HISTO_COLOR = "#6497b1";
 const romaO = ["#733957", "#743956", "#753954", "#753853", "#763851", "#773850", "#77384f", "#78384d", "#79384c", "#79384b", "#7a3849", "#7b3848", "#7c3847", "#7c3946", "#7d3945", "#7e3943", "#7e3942", "#7f3a41", "#803a40", "#813b3f", "#813b3e", "#823c3d", "#833c3c", "#843d3b", "#843d3a", "#853e39", "#863f38", "#874037", "#874037", "#884136", "#894235", "#8a4334", "#8b4433", "#8c4533", "#8c4632", "#8d4731", "#8e4831", "#8f4930", "#904a30", "#914c2f", "#924d2f", "#934e2e", "#94502e", "#94512d", "#95522d", "#96542d", "#97552c", "#98572c", "#99582c", "#9a5a2c", "#9b5b2c", "#9c5d2b", "#9d5f2b", "#9e602b", "#9f622b", "#a0642c", "#a2662c", "#a3672c", "#a4692c", "#a56b2d", "#a66d2d", "#a76f2d", "#a8712e", "#a9732e", "#aa752f", "#ab7730", "#ad7930", "#ae7b31", "#af7d32", "#b07f33", "#b18134", "#b28335", "#b48636", "#b58837", "#b68a39", "#b78c3a", "#b88e3b", "#b9913d", "#bb933f", "#bc9540", "#bd9842", "#be9a44", "#bf9c46", "#c19f47", "#c2a149", "#c3a34b", "#c4a54e", "#c5a850", "#c6aa52", "#c8ac54", "#c9af57", "#cab159", "#cbb35c", "#ccb55e", "#cdb761", "#ceba63", "#cfbc66", "#cfbe68", "#d0c06b", "#d1c26e", "#d2c470", "#d2c673", "#d3c876", "#d4c978", "#d4cb7b", "#d4cd7e", "#d5ce81", "#d5d083", "#d5d186", "#d6d388", "#d6d48b", "#d6d58e", "#d6d790", "#d6d893", "#d5d995", "#d5da98", "#d5db9a", "#d4dc9c", "#d4dd9f", "#d3dda1", "#d3dea3", "#d2dfa5", "#d1dfa7", "#d0e0a9", "#cfe0ab", "#cee0ad", "#cde1af", "#cce1b1", "#cbe1b3", "#cae1b5", "#c8e1b6", "#c7e1b8", "#c5e1b9", "#c4e1bb", "#c2e1bc", "#c1e1be", "#bfe1bf", "#bde0c0", "#bbe0c2", "#b9dfc3", "#b8dfc4", "#b6dec5", "#b4dec6", "#b1ddc7", "#afdcc8", "#addcc8", "#abdbc9", "#a9daca", "#a7d9cb", "#a4d8cb", "#a2d7cc", "#a0d6cc", "#9dd5cd", "#9bd4cd", "#99d3ce", "#96d1ce", "#94d0ce", "#92cfce", "#8fcecf", "#8dcccf", "#8bcbcf", "#88c9cf", "#86c8cf", "#84c6cf", "#81c5cf", "#7fc3cf", "#7dc2ce", "#7bc0ce", "#78bece", "#76bdce", "#74bbcd", "#72b9cd", "#70b8cd", "#6eb6cc", "#6cb4cc", "#6ab2cb", "#68b1cb", "#67afca", "#65adca", "#63abc9", "#62a9c9", "#60a8c8", "#5fa6c7", "#5da4c7", "#5ca2c6", "#5aa0c5", "#599ec4", "#589cc4", "#579bc3", "#5699c2", "#5597c1", "#5495c0", "#5393bf", "#5291be", "#528fbd", "#518dbc", "#508bbb", "#508aba", "#4f88b9", "#4f86b8", "#4f84b7", "#4f82b6", "#4e80b4", "#4e7eb3", "#4e7cb2", "#4e7ab0", "#4f78af", "#4f76ae", "#4f75ac", "#4f73ab", "#5071a9", "#506fa8", "#516da6", "#516ba4", "#5269a3", "#5267a1", "#53669f", "#54649e", "#54629c", "#55609a", "#565f98", "#575d96", "#575b94", "#585993", "#595891", "#5a568f", "#5b558d", "#5c538b", "#5c5289", "#5d5087", "#5e4f85", "#5f4d83", "#604c81", "#614b7f", "#62497d", "#63487b", "#634779", "#644677", "#654576", "#664474", "#674372", "#684270", "#68416e", "#69406c", "#6a3f6b", "#6b3f69", "#6c3e67", "#6c3d65", "#6d3d64", "#6e3c62", "#6f3b60", "#6f3b5f", "#703a5d", "#713a5c", "#723a5a", "#723959"];
 const colormap = d3.interpolateRgbBasis(romaO);
 
@@ -16,8 +17,6 @@ class Bundling {
             var X = edge.X;
             var Y = edge.Y;
 
-            //var angle = 
-
             var polyline = []
             for(var i = 0; i < X.length; i++) {
                 polyline.push({'x': X[i], 'y': Y[i]})
@@ -34,7 +33,7 @@ class Bundling {
         }
 
         const zoom = d3.zoom().on('zoom', handleZoom);
-        this.svg.call(zoom);
+        // this.svg.call(zoom);
         
         this.draw_area.selectAll('.edge')
             .data(this.edges)
@@ -60,8 +59,12 @@ class Bundling {
         var b_width = extentX[1] - extentX[0]
         var b_height = extentY[1] - extentY[0] 
 
+        console.log(b_width, b_height)
+
         var width = this.svg.node().getBoundingClientRect().width;
         var height = width * b_height / b_width;
+
+        console.log(width, height)
 
         this.svg.attr('height', height);
 
@@ -101,14 +104,14 @@ class Histogram{
         var mean = d3.mean(edges, d => d[accessor]);
         var median = d3.median(edges, d => d[accessor]);
 
-        this.stats.append('span').text(`mean: ${mean.toFixed(2)}   median: ${median.toFixed(2)}`)
+        this.stats.append('span').text(`mean: ${mean.toFixed(3)}   median: ${median.toFixed(3)}`)
 
     }
 
     async draw () {
         this.svg.selectAll('*').remove();
         var width = this.svg.node().getBoundingClientRect().width;
-        this.svg.attr('height', width);
+        this.svg.attr('height', 300);
 
         var xScale = d3.scaleLinear().domain(this.extentX).range([3 * margin, width - margin]);
 
@@ -135,7 +138,7 @@ class Histogram{
         .append("rect")
             .attr('x', 1)
             .attr('y', 0)
-            .style("fill", "#69b3a2")
+            .style("fill", HISTO_COLOR)
 
         this.resize();
     }
@@ -184,7 +187,7 @@ class TextElement{
         this.accessor = accessor;
 
  
-        this.container.append("span").text(this.name + ": " + this.data[accessor].toFixed(3));
+        this.container.text(this.name + ": " + this.data[accessor].toFixed(3));
     }
 
     async draw () {
@@ -270,6 +273,8 @@ class Container{
 
     metric_visibility(metric, flag) {
         var mC = this.metrics[metric]
+
+        console.log(metric);
 
         mC.container.style('display', flag ? 'block' : 'none');
         mC.container.style('visibility', flag ? 'visible' : 'collapse');
